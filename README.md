8ea71f6fa66f9ea1347eaecd26a6ffd7e7ae87b2
mohamadassaf <mohammad.assaf@live.com>
2020-03-14 18:44:59 +0300

    add -add new item- feature
    Can input a shopping item, submit to append list

---
+++ README.md
@@ -0,0 +1,13 @@
+# Shopping List Management
+## Description
+ReactJS web application. User inputs a list of shopping items, and can interactively search through them.
+## How to run
+* clone this repository
+* Run 
+```
+npm start
+```
+* View interface on
+```
+localhost:3000
+```
---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Shopping List</h3>
+        <ShoppingList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new item">
+            What do you need to buy?
+          </label>
+          <input
+            id="new item"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>
+            Add #{this.state.items.length + 1}
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default App;
